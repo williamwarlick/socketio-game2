f907e7851c519d914cc5ac922339a5b98f720163
Erik Pearson <epearson@bowdoin.edu>
2023-04-26 12:56:48 -0400

    misc updates for tracking cursor

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
@@ -8,38 +9,57 @@ const { InMemorySessionStore } = require("./sessionStore");
 const crypto = require("crypto");
 const randomId = () => crypto.randomBytes(8).toString("hex");
 const mab = require('./moveablock-server');
+const cookieParser = require('cookie-parser');
 
 const sessionStore = new InMemorySessionStore();
 const gameServer = new mab.GameServer();
 
+const sessionMiddleware = session({
+    secret: 'mySecret',
+    resave: false,
+    saveUninitialized: true,
+});
+
+io.engine.use(sessionMiddleware);
+
 app.use(express.static('dist'));
 
 io.on('connection', async (socket) => {  
-    console.log('A user connected ...' + socket.id);
-    gameServer.joinMoveABlock(io, socket.id);
+    // get the session from the socket
+    const session = socket.request.session;
+    let username;
+
+    if (!session.username) {
+        // get the username from the query string
+        username = randomId(); //socket.handshake.query.username;
+
+        // store the username in the session
+        session.username = username;
+
+        session.save();
+    }
+    
+    console.log('A user connected ...' + username);
+    gameServer.joinMoveABlock(io, username);
 
     socket.on('lobby', (msg) => {
         console.log('message: ' + msg);
     });
 
-    socket.on('joinRoom1', (msg) => {
-        console.log('Join room1: ' + msg);
-        socket.join('room1');
-    });
-
-    socket.on('pingRoom1', (msg) => {
-        console.log('ping: ' + msg);
-        io.to('room1').emit('Pong!');
-    });
-
     socket.on('moveablock', (msg) => {
+        // get the session from the socket
+        const session = socket.request.session;
+
+        // get the username from the session
+        const username = session.username;
+
         console.log('moveablock: ' + JSON.stringify(msg));
 
-        var game = gameServer.getGameByPlayerId(socket.id);
+        var game = gameServer.getGameByPlayerId(username);
 
         if (game) {
             if (msg.event === mab.EVENTS.DROP) {
-                gameServer.move(socket, socket.id, msg);
+                gameServer.move(socket, username, msg);
             } else if (msg.event === mab.EVENTS.DRAGOVER || msg.event === mab.EVENTS.DRAGSTART) {
                 msg.state = null;
                 socket.broadcast.emit('moveablock', msg);
@@ -47,7 +67,7 @@ io.on('connection', async (socket) => {
                 console.log('Message event does not match.');
             }
         } else {
-            console.log("Could not find game for player id: " + socket.id);
+            console.log("Could not find game for player id: " + username);
         }
     });
 });
