0acab786716b8b3ec70fdde6f5631b339d99703c
Michael Coleman <colemanmc02@gmail.com>
2023-10-18 04:33:19 -0400

    feedback adjustments

--- src/js/game-complete.js
+++ src/js/game-complete.js
@@ -1,25 +1,28 @@
-import header from './header';
-import template from '../templates/game-complete.mustache';
+import header from './header'
+import template from '../templates/game-complete.mustache'
 
-(async function doRender(){
-    const response = await fetch('/gamestate');
-    const data = await response.json();
+;(async function doRender() {
+	const response = await fetch('/gamestate')
+	const data = await response.json()
 
-    data.rounds = data.rounds.map((round, index) => ({...round, roundNum: index}));
+	data.rounds = data.rounds.map((round, index) => ({
+		...round,
+		roundNum: index,
+	}))
 
-    if (data.user.sonaId) {
-        data.hasSonaId = true;
-    } else {
-        data.hasSonaId = false;
-    }
+	if (data.user.sonaId) {
+		data.hasSonaId = true
+	} else {
+		data.hasSonaId = false
+	}
 
-    // Set the rendered HTML as the content of the page
-    const element = document.getElementById('game-results');
+	// Set the rendered HTML as the content of the page
+	const element = document.getElementById('game-results')
 
-    element.insertAdjacentHTML('beforeend', template(data));
+	element.insertAdjacentHTML('beforeend', template(data))
 
-    // log out user
-    fetch('/logout', {
-        method: "POST"
-    });
-})();
+	// log out user
+	fetch('/logout', {
+		method: 'POST',
+	})
+})()
